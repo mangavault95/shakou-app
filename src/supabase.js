b8688ddb59c32c3d510de06eeeb387638cc7c736
MangaVault10x <cmoaziendale@gmail.com>
2026-06-03 14:02:56 +0200

    Refactor supabase client initialization

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+export default supabase;
+
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
